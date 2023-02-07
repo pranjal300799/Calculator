347570717d3d2c6885f0f264ac3d323ec9e964fc
pranjal300799 <pranjalkumarkunik@gmail.com>
2023-02-08 00:23:02 +0530

    Add functions for basic arthmetic operations
    Add functions for addition,subtraction,multiplication,division
    and modulo

---
+++ script.js
@@ -0,0 +1,25 @@
+function addTwoNumbers(a,b)
+{
+    let sum=a+b;
+    return result;
+}
+function subtractTwoNUmber(a,b)
+{
+    let result=a-b;
+    return result;
+}
+function multiplyTwoNumber(a,b)
+{
+    let result=a*b;
+    return result;
+}
+function divideTwoNUmbers(a,b)
+{
+    let result=(a*1.0)/b;
+    return result;
+}
+function giveRemainder(a,b)
+{
+    let result =a%b;
+    return result;
+}
